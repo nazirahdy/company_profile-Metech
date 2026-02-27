7ead753ba929007ceba69a31a7d9bc72abc7e484
HauraAdisty <162139041+HauraAdisty@users.noreply.github.com>
2026-02-27 09:22:33 +0700

    fix error

--- company-metech/app/Providers/AppServiceProvider.php
+++ company-metech/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Number; // Tambahkan baris ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan baris ini untuk mematikan ketergantungan pada ekstensi intl
+        Number::useLocale('en');
     }
 }
